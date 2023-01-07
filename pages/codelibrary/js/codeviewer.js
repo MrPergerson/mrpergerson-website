13ced828ef2674cc47b9578471e3b18b26779a69
Cole Pergerson <cole.pergerson@gmail.com>
2023-01-06 20:45:33 -0800

    finished code view functionality and visual design 1

--- pages/codelibrary/js/codeviewer.js
+++ pages/codelibrary/js/codeviewer.js
@@ -1,9 +1,26 @@
 
-
-fetch('../unity/src/PlayerController.cs')
+function getCode(link)
+{
+  fetch(link)
   .then(response => response.text())
   .then(data => {
     // Store the C# code in a variable
-    const code = data;
+    let code = data;
+    code = cleanUpCode(code);
     document.getElementById('code-view').innerHTML = code;
+
+    hljs.highlightAll();
   });
+
+}
+
+
+function cleanUpCode(code)
+{
+
+  code = code.replaceAll("<","&lt;");
+  code = code.replaceAll(">","&gt;");
+  code = code.replaceAll("\"","&quot;");
+
+  return code;
+}
--- pages/codelibrary/unity/src/PlayerController.cs
+++ pages/codelibrary/unity/src/PlayerController.cs
@@ -1,79 +1,73 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-namespace cameraman
+[RequireComponent(typeof(PlayerInput))]
+public class PlayerController : MonoBehaviour
 {
+    
+    private TopDownMovement move;
+    private Controls controls;
 
-    [RequireComponent(typeof(PlayerInput))]
-    public class PlayerController : MonoBehaviour
+    PlayerInput playerInputComponent;
+
+    [SerializeField] bool usingGamepad = false;
+
+    private float autoTurnTimer = .3f;
+    private float timeSinceManualTurn = 0;
+
+    private void Awake()
     {
-        
-        private TopDownMovement move;
-        private Controls controls;
+        controls = new Controls();
+        playerInputComponent = GetComponent<PlayerInput>();
 
-        PlayerInput playerInputComponent;
+        move = GetComponent<TopDownMovement>();
 
-        [SerializeField] bool usingGamepad = false;
-
-        private float autoTurnTimer = .3f;
-        private float timeSinceManualTurn = 0;
-
-        private void Awake()
-        {
-            controls = new Controls();
-            playerInputComponent = GetComponent<PlayerInput>();
-
-            move = GetComponent<TopDownMovement>();
-
-            playerInputComponent.onControlsChanged += UpdateCurrentDevice;
-            usingGamepad = playerInputComponent.currentControlScheme.Equals("Gamepad") ? true : false;
-        }
-
-        private void OnEnable()
-        {
-            controls.Enable();
-        }
-
-        private void OnDisable()
-        {
-            controls.Disable();
-        }
-
-        private void Update()
-        {
-            UpdateMovement();
-        }
-
-        private void UpdateMovement()
-        {
-            move.Move(controls.Player.Move.ReadValue<Vector2>());
-
-            if (controls.Player.Look.WasPerformedThisFrame())
-            {
-                timeSinceManualTurn = Time.time;
-
-                var look = controls.Player.Look.ReadValue<Vector2>();
-
-                if (usingGamepad)
-                    move.RotateUsingGamepad(look);
-                else
-                    move.RotateUsingMouse(look);
-            }         
-            else if (move.IsMoving && Time.time - timeSinceManualTurn > autoTurnTimer)
-            {
-                var move = controls.Player.Move.ReadValue<Vector2>();
-                var dir = new Vector3(move.x, 0, move.y);
-                this.move.RotateTowardsDirection(dir);
-            }
-        }
-
-        private void UpdateCurrentDevice(PlayerInput input)
-        {
-            usingGamepad = input.currentControlScheme.Equals("Gamepad") ? true : false;
-        }
-
-
+        playerInputComponent.onControlsChanged += UpdateCurrentDevice;
+        usingGamepad = playerInputComponent.currentControlScheme.Equals("Gamepad") ? true : false;
     }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+    private void Update()
+    {
+        UpdateMovement();
+    }
+
+    private void UpdateMovement()
+    {
+        move.Move(controls.Player.Move.ReadValue<Vector2>());
+
+        if (controls.Player.Look.WasPerformedThisFrame())
+        {
+            timeSinceManualTurn = Time.time;
+
+            var look = controls.Player.Look.ReadValue<Vector2>();
+
+            if (usingGamepad)
+                move.RotateUsingGamepad(look);
+            else
+                move.RotateUsingMouse(look);
+        }         
+        else if (move.IsMoving && Time.time - timeSinceManualTurn > autoTurnTimer)
+        {
+            var move = controls.Player.Move.ReadValue<Vector2>();
+            var dir = new Vector3(move.x, 0, move.y);
+            this.move.RotateTowardsDirection(dir);
+        }
+    }
+
+    private void UpdateCurrentDevice(PlayerInput input)
+    {
+        usingGamepad = input.currentControlScheme.Equals("Gamepad") ? true : false;
+    }
+
+
 }
